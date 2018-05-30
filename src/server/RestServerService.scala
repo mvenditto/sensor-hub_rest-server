175c6381f92b109cd2dddfefdf2d7daca5ea86d4
mvenditto <venditto.matteo@gmail.com>
2018-05-30 20:33:05 +0200

    Restricted Javalin logging to FATAL level only.

--- src/server/RestServerService.scala
+++ src/server/RestServerService.scala
@@ -1,10 +1,13 @@
 package server
 
+import org.apache.log4j.{Level, Logger}
 import spi.service.{Service, ServiceMetadata}
 
 class RestServerService extends Service {
 
   System.setProperty("org.eclipse.jetty.LEVEL", "OFF")
+  Logger.getLogger("io.javalin").setLevel(Level.FATAL)
+
 
   override def init(metadata: ServiceMetadata): Unit =
     JavalinServer.main(Array(metadata.rootDir))
